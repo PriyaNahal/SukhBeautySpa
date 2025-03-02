62b9973d42d7bc25eaf8ff6072e0fb9958aa4572
PriyaNahal <83884139+PriyaNahal@users.noreply.github.com>
2025-03-01 20:04:52 -0800

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+<html>
+<footer>
+        <p>&copy; 2025 Sukh Beauty Spa. All Rights Reserved.</p>
+    </footer>
+</html>
+
+<style>
+        footer {
+        background-color: white;
+        color: black;
+        text-align: center;
+        padding: 1em 0;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
